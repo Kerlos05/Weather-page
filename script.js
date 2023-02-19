c74c987421cdc951d2cbdba2df9e68c630f7d6d2
Kerlos05 <96844802+Kerlos05@users.noreply.github.com>
2023-02-19 18:15:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const btn  = document.getElementById('btn');
 const input = document.getElementById('input');
-const MyIcon = document.getElementById('myIcon');
 const output = document.querySelector('.output')
-const container = document.querySelector('.container')
+const container = document.querySelector('.container-style')
+const forcastDisplayer = document.querySelector('#forcastDisplayer'); 
+
 
 btn.addEventListener('click', function(e) {
 e.preventDefault()
-if(!input.value){
+if(input.value == ''){
     return 
 }
 fetch('https://api.weatherapi.com/v1/current.json?key=b83d999ff3eb430ca00160502230302&q=' + input.value+'&aqi=no')
@@ -14,38 +15,88 @@ fetch('https://api.weatherapi.com/v1/current.json?key=b83d999ff3eb430ca001605022
 .then((data) => {
     const wrapper = document.createElement('div');
     const temp = document.createElement('p');
-    const con = document.createElement('p');
     const local = document.createElement('p');
-    const image = document.createElement('img');
 
     const {
         location: {name,localtime}, 
-        current:{temp_c},
         current:{
-            condition:{icon, text, code}
+            temp_c,
+            condition:{text, code}
         }
     } = data
 
-    image.src = icon; 
 
     wrapper.classList.add('dis'); 
 
-    con.append(image);
     temp.append(temp_c + '°C    ' + name);
     local.append(localtime + '  ' + text);
  
-    wrapper.append(local); 
-    wrapper.append(con);
     wrapper.append(temp);
+    wrapper.append(local); 
 
     if(output.hasChildNodes){
         output.innerHTML = ''; 
     }
 
     output.append(wrapper);
+    
+    // Display weather image
+    checkWeather(data.current.is_day, code)
 
-    if(!data.current.is_day){
-        document.body.style.color = 'white'
+    // Forecast
+    getForcast(name); 
+})
+
+.catch(err =>{
+    console.log(err);
+})
+
+input.value = '';
+document.body.classList.toggle('open'); 
+})
+
+window.onload = () =>{
+        navigator.geolocation.getCurrentPosition(function(position){
+        fetch('https://api.weatherapi.com/v1/current.json?key=b83d999ff3eb430ca00160502230302&q=' +`${position.coords.latitude} + ${position.coords.longitude}`)
+        .then((res) => res.json())    
+        .then((data) => {
+            const wrapper = document.createElement('div');
+            const temp = document.createElement('p');
+            const local = document.createElement('p');
+        
+            const {
+                location: {name,localtime}, 
+                current:{
+                    temp_c,
+                    condition:{text, code}
+                }
+            } = data
+        
+            wrapper.classList.add('dis'); 
+
+            temp.append(`${temp_c}°C ${name}`);
+            local.append(`${localtime} ${text}`);
+
+            wrapper.append(temp);
+            wrapper.append(local); 
+        
+            if(output.hasChildNodes){
+                output.innerHTML = ''; 
+            }
+
+            output.append(wrapper);
+
+             // Display weather image
+            checkWeather(data.current.is_day, code); 
+            
+              // Forecast
+            getForcast(name); 
+        })
+    }); 
+}
+
+function checkWeather(is_day, code){
+    if(!is_day){
 
         // Clear weather
         container.style.color = 'aliceblue'
@@ -97,8 +148,7 @@ fetch('https://api.weatherapi.com/v1/current.json?key=b83d999ff3eb430ca001605022
 
     }
 
-    if(data.current.is_day){
-        document.body.style.color = 'white'
+    if(is_day){
 
         // Clear weather
         container.style.color = 'black'
@@ -148,159 +198,54 @@ fetch('https://api.weatherapi.com/v1/current.json?key=b83d999ff3eb430ca001605022
             container.style.backgroundImage = `url(./weather-condition-images/day/Snowy.jpg)`
         }
     }
+}
 
-})
 
-.catch(err =>{
-    console.log(err);
-})
-
-input.value = '';
-document.body.classList.toggle('open'); 
-})
-
-window.onload = () =>{
-        navigator.geolocation.getCurrentPosition(function(position){
-        fetch('https://api.weatherapi.com/v1/current.json?key=b83d999ff3eb430ca00160502230302&q=' +`${position.coords.latitude} + ${position.coords.longitude}`)
-        .then((res) => res.json())    
-        .then((data) => {
-            const wrapper = document.createElement('div');
-            const temp = document.createElement('p');
-            const con = document.createElement('p');
-            const local = document.createElement('p');
-            const image = document.createElement('img');
-        
-            const {
-                location: {name,localtime}, 
-                current:{temp_c},
-                current:{
-                    condition:{icon, text, code}
-                }
-            } = data
-        
-            image.src = icon; 
-        
-            wrapper.classList.add('dis'); 
-
-            temp.append(temp_c + '°C    ' + name);
-            local.append(localtime + '  ' + text);
-            con.append(image);
-
-            wrapper.append(local); 
-            wrapper.append(con);
-            wrapper.append(temp);
-        
-            if(output.hasChildNodes){
-                output.innerHTML = ''; 
+function getForcast(name) {
+    forcastDisplayer.innerHTML = ''
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=b83d999ff3eb430ca00160502230302&q=${name}&days=3&aqi=no&alerts=no`)
+        .then(resp => resp.json())
+        .then(data => {
+            const forecast = data.forecast.forecastday[0];
+            const localtime = data.location.localtime.split(' ')[1]; // extract time portion only
+      
+            for (let i = 0; i <= 12; i++) {
+              const temp_c = forecast.hour[i].temp_c;
+              const icon = forecast.hour[i].condition.icon;
+      
+              const wrapper = document.createElement('div');
+              wrapper.classList.add('forecast-item');
+      
+              const temp = document.createElement('p');
+              temp.innerText = temp_c;
+      
+              const time = document.createElement('p');
+              time.innerText = formatTime(localtime, i);
+      
+              const iconElement = document.createElement('img');
+              iconElement.src = icon;
+      
+              wrapper.append(iconElement);
+              wrapper.append(time);
+              wrapper.append(temp);
+      
+              forcastDisplayer.append(wrapper);
             }
-            output.append(wrapper);
-            console.log(data);
+            
+        });
 
+     
+}
 
-            if(!data.current.is_day){
-                document.body.style.color = 'white'
-                // Clear weather
-                if(code == 1000){
-                    container.style.backgroundImage = 'url(./weather-condition-images/night/Clear.jpg)'
-                }
-                
-                // cloudy
-                else if(
-                    code == 1003 || 
-                    code == 1006 ||
-                    code == 1009 ||
-                    code == 1030 ||
-                    code == 1069 ||
-                    code == 1087 ||
-                    code == 1135 ||
-                    code == 1273 ||
-                    code == 1276 ||
-                    code == 1279 ||
-                    code == 1282
-                ){
-                    container.style.backgroundImage = `url(./weather-condition-images/night/Cloudy.jpg)`
-                }
-                
-                // //Rain
-                else if(
-                    code == 1063 ||
-                    code == 1069 ||
-                    code == 1072 ||
-                    code == 1150 ||
-                    code == 1180 ||
-                    code == 1183 ||
-                    code == 1186 ||
-                    code == 1189 ||
-                    code == 1192 ||
-                    code == 1204 ||
-                    code == 1207 ||
-                    code == 1240 ||
-                    code == 1243 ||
-                    code == 1246 ||
-                    code == 1249 ||
-                    code == 1252 
-                ){
-                    container.style.backgroundImage = `url(./weather-condition-images/night/Rainy.jpg)`
-                }
-                else{
-                    container.style.backgroundImage = `url(./weather-condition-images/night/Snowy.jpg)`
-                }
-            }
+function formatTime(localtime, increment) {
+    let [hours, minutes] = localtime.split(':').map(Number);
+    hours = (hours + increment) % 24;
 
-            if(data.current.is_day){
-                document.body.style.color = 'black'
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
 
-
-                // Clear weather
-                if(code == 1000){
-                    container.style.backgroundImage = 'url(./weather-condition-images/day/Clear.jpg)'
-                    alert()
-                }
-                
-                // cloudy
-                else if(
-                    code == 1003 || 
-                    code == 1006 ||
-                    code == 1009 ||
-                    code == 1030 ||
-                    code == 1069 ||
-                    code == 1087 ||
-                    code == 1135 ||
-                    code == 1273 ||
-                    code == 1276 ||
-                    code == 1279 ||
-                    code == 1282
-                ){
-                    container.style.backgroundImage = `url(./weather-condition-images/day/Cloudy.jpg)`
-                }
-                
-                // //Rain
-                else if(
-                    code == 1063 ||
-                    code == 1069 ||
-                    code == 1072 ||
-                    code == 1150 ||
-                    code == 1180 ||
-                    code == 1183 ||
-                    code == 1186 ||
-                    code == 1189 ||
-                    code == 1192 ||
-                    code == 1204 ||
-                    code == 1207 ||
-                    code == 1240 ||
-                    code == 1243 ||
-                    code == 1246 ||
-                    code == 1249 ||
-                    code == 1252 
-                ){
-                    container.style.backgroundImage = `url./weather-condition-images/day/Rainy.jpg)`
-                }
-                else{
-                    container.style.backgroundImage = `url(./weather-condition-images/day/Snowy.jpg)`
-                }
-            }
-
-        })
-    }); 
+  return `${hours}:${minutes.toString().padStart(2, '0')}`;
 
 }
+
